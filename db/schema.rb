51545584709cac555b404962961d1e4139aba669
Jonan Scheffler <jonanscheffler@gmail.com>
2017-04-21 23:47:04 -0700

    Remove null:false from User email so bots can make users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421010109) do
+ActiveRecord::Schema.define(version: 20170422063104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170421010109) do
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.string   "name",       null: false
-    t.string   "email",      null: false
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
